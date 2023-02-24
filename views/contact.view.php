d23e936abf665d31fceca5496ca057b0a4f5496c
bagashiz <work.bitsy@simplelogin.com>
2023-02-24 09:32:43 +0700

    add html

---
+++ views/contact.view.php
@@ -0,0 +1,11 @@
+<?php require 'partials/head.view.php' ?>
+<?php require 'partials/nav.view.php' ?>
+<?php require 'partials/header.view.php' ?>
+
+<main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <h1>Contact</h1>
+    </div>
+</main>
+
+<?php require 'partials/footer.view.php' ?>
