b32e044802499469d6590b5bb56d565dc4216a12
bagashiz <work.bitsy@simplelogin.com>
2023-02-25 13:40:46 +0700

    change partial file naming

--- views/404.view.php
+++ views/404.view.php
@@ -1,5 +1,5 @@
-<?php require 'partials/head.view.php' ?>
-<?php require 'partials/nav.view.php' ?>
+<?php require 'partials/head.php' ?>
+<?php require 'partials/nav.php' ?>
 
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
@@ -11,4 +11,4 @@
     </div>
 </main>
 
-<?php require 'partials/footer.view.php' ?>
+<?php require 'partials/footer.php' ?>
